fc958bb3fe62b8db5b8ffa4144c571aede4af906
Cleiton-D <32417778+Cleiton-D@users.noreply.github.com>
2020-08-09 03:29:14 +0000

    Deploying to storybook-pages from  @ b0246c636fb9c29a038e4cb5140f7fe8c7b5a99d 🚀

---
+++ precache-manifest.29d5b3256ad306bc205e2469c35ba4d8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c60a4dc01fa958e88e1ef3c9936e1f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.4e993e8c2e36971011eb.bundle.js"
+  },
+  {
+    "url": "runtime~main.4e993e8c2e36971011eb.bundle.js"
+  },
+  {
+    "url": "vendors~main.4e993e8c2e36971011eb.bundle.js"
+  },
+  {
+    "url": "vendors~main.4e993e8c2e36971011eb.bundle.js.LICENSE.txt"
+  }
+]);
